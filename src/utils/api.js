c71bf8d9eac0cd24a14c7a05f4cce40a1dd3a230
AlinaBielska <alinkabelka5@gmail.com>
2023-07-13 17:12:48 +0200

    install axios

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,48 +1,38 @@
 import axios from 'axios';
 
 class apiFetch {
-  #BASE_URL = 'https://api.themoviedb.org/3/';
-  #API_KEY = '0a4aab5daca4d3b8b09b771948ad9265';
-  #SEARCH = 'search/movie';
-  #TRAND = 'trending/movie/day';
-  #ALL = 'movie/';
-
-  /* Запрос на ТРЕНДЫ */
+  
   trend = async page => {
-    const respons = await axios.get(
-      `${this.#BASE_URL}${this.#TRAND}?api_key=${this.#API_KEY}&page=${page}`
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=41e82972d79cb88ec6adf4cd9cbe70d2&page=${page}`
     );
-    return respons.data.results;
+    return response.data.results;
   };
 
-  /* Запрос ПОИСК ИНПУТ */
   search = async (query, page) => {
-    const respons = await axios.get(
-      `${this.#BASE_URL}${this.#SEARCH}?api_key=${this.#API_KEY}&query=${query}&page=${page}`
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/movie?api_key=41e82972d79cb88ec6adf4cd9cbe70d2&query=${query}&page=${page}`
     );
-    return respons.data;
+    return response.data;
   };
 
-  /* Запрос id */
   id = async id => {
-    const respons = await axios.get(`${this.#BASE_URL}${this.#ALL}/${id}?api_key=${this.#API_KEY}`);
-    return respons.data;
+    const response = await axios.get(`https://api.themoviedb.org/3/movie//${id}?api_key=41e82972d79cb88ec6adf4cd9cbe70d2`);
+    return response.data;
   };
 
-  /* Запрос Cast */
   cast = async id => {
-    const respons = await axios.get(
-      `${this.#BASE_URL}${this.#ALL}/${id}/credits?api_key=${this.#API_KEY}`
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie//${id}/credits?api_key=41e82972d79cb88ec6adf4cd9cbe70d2`
     );
-    return respons.data.cast;
+    return response.data.cast;
   };
 
-  /* Запрос Reviews */
   reviews = async id => {
-    const respons = await axios.get(
-      `${this.#BASE_URL}${this.#ALL}/${id}/reviews?api_key=${this.#API_KEY}`
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie//${id}/reviews?api_key=41e82972d79cb88ec6adf4cd9cbe70d2`
     );
-    return respons.data.results;
+    return response.data.results;
   };
 }
 
